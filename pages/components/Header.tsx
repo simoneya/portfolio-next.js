8c9152d7c4dd4d25f72b3090d1b905865c5103b9
simoneya <yavar.simona@yahoo.com>
2023-03-17 13:45:59 +0000

    working on framer-motion animation

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,41 +1,48 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion";
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0 flex items-start">
-        <div className="flex flex-row items-center">
+    <header className="sticky top-0 pt-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl-items-center">
+        <motion.div 
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          className="flex flex-row items-center">
             {/* Social Icons */}
             <SocialIcon 
              url="https://twitter.com" 
-             fgColor="cornflowerblue" 
+             fgColor="lightblue" 
              bgColor="transparent" 
             />
 
             <SocialIcon 
              url="https://instagram.com" 
-             fgColor="cornflowerblue" 
+             fgColor="lightblue" 
              bgColor="transparent" 
             />
 
             <SocialIcon 
              url="https://facebook.com" 
-             fgColor="cornflowerblue" 
+             fgColor="lightblue"  
              bgColor="transparent" 
             />
-        </div>
+        </motion.div>
 
-        <div className="flex flex-row items-center text-blue-400 cursor-pointer">
+        <div className="flex flex-row items-center text-blue-200 cursor-pointer">
             <SocialIcon
              className="cursor-pointer"
              network="email"
-             fgColor="cornflowerblue" 
+             fgColor="lightblue" 
              bgColor="transparent"
             />
 
-            <p className="uppercase hidden md:inline-flex text-sm text-blue-500">Get In Touch</p>
+            <p className="uppercase hidden md:inline-flex text-sm text-blue-200">Get In Touch</p>
         </div>
 
     </header>
