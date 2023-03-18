446990aae360fd3a64b881b851ad12b1407d90e1
simoneya <yavar.simona@yahoo.com>
2023-03-18 15:55:43 +0000

    implementing typewriter

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 type Props = {}
 
 export default function Hero({}: Props) {
+
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, the Name's Simona", 
+            "Girl-who-codes", 
+            "<FrontEndDeveloper />"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+
   return (
-    <div>Hero</div>
-  )
+    <div>
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor="#ffc0cb"/> 
+        </h1>
+    </div>
+  );
 }
