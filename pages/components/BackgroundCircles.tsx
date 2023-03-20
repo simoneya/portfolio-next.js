8c70920a15f14db4be887ad53e0aeb3d95ae8db4
simoneya <yavar.simona@yahoo.com>
2023-03-20 14:39:12 +0000

    fixed background circles effect

--- pages/components/BackgroundCircles.tsx
+++ pages/components/BackgroundCircles.tsx
@@ -18,11 +18,11 @@ export default function BackgroundCircles({}: Props) {
           duration: 2.5,
       }}
         className="relative flex justify-center items-center"> 
-        <div className="absolute border border-[#D1456D] rounded-full h-[200px] w-[200px] mt-52 animate-ping"/>
-        <div className="rounded-full border border-[#D1456D] h-[300px] w-[300px] absolute mt-52"/>
-        <div className="rounded-full border border-[#D1456D] h-[500px] w-[500px] absolute mt-52"/>
+        <div className="absolute border border-[#FF93A0] rounded-full h-[200px] w-[200px] mt-52 animate-ping"/>
+        <div className="rounded-full border border-[#CC6666] h-[300px] w-[300px] absolute mt-52"/>
+        <div className="rounded-full border border-[#CC6666] h-[500px] w-[500px] absolute mt-52"/>
         <div className="rounded-full border border-[#5AD1AF] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse"/>
-        <div className="rounded-full border border-[#D1456D] h-[800px] w-[800px] absolute mt-52"/>
+        <div className="rounded-full border border-[#CC6666] h-[800px] w-[800px] absolute mt-52"/>
     </motion.div>
   );
 }
