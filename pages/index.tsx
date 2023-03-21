ddecb38496bab33ede20a1557625df65e8a87c86
simoneya <yavar.simona@yahoo.com>
2023-03-21 12:46:37 +0000

    making about component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,11 +22,14 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero" className="snap">
+      <section id="hero" className="snap-start">
         <Hero />
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
