e41c46ecc97276cffefd3305911ef48d93c736f1
simoneya <yavar.simona@yahoo.com>
2023-03-22 11:33:40 +0000

    update

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
 
 type Props = {}
 
 export default function About({}: Props) {
   return (
     <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-        <h3>About</h3>
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">About</h3>
+
+        <motion.img
+          initial={{
+            x: -200,
+          }}
+          transition={{
+              duration: 1.2,
+          }}
+          whileInView={{
+            x: 0,
+          }}
+          viewport={{
+              once: true,
+          }}
+            src="https://lh3.googleusercontent.com/a/AGNmyxbd_T3xwm6eR7GhsO5bufmMLgTUWZEEZWfVybzG=s576" alt="" />
     </div>
   )
 }
