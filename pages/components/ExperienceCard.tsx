89bf9ca1828b8bebe5991b85914733e7c1f15031
simoneya <yavar.simona@yahoo.com>
2023-03-23 11:14:50 +0000

    update on article div

--- pages/components/ExperienceCard.tsx
+++ pages/components/ExperienceCard.tsx
@@ -15,6 +15,23 @@ export default function ExperienceCard({}: Props) {
         src="https://i.pinimg.com/280x280_RS/b4/ca/74/b4ca74ef398238998747843bc819ddbc.jpg" 
         alt="" 
       />
+
+      <div>
+        <h4>Junior Developer</h4>
+        <p>Cubicod</p>
+        <div>
+          {/* Tech used */}
+          {/* Tech used */}
+          {/* Tech used */}
+        </div>
+        <p>Started work...- Ended...</p>
+        <ul className="list-disc space-y-4 ml-5 text-lg">
+          <li>summary points</li>
+          <li>summary points</li>
+          <li>summary points</li>
+          <li>summary points</li>
+        </ul>
+      </div>
     </article>
   );
 }
