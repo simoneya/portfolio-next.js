b2823d7a694536216f8ff6b4546d56b1a946eac6
simoneya <yavar.simona@yahoo.com>
2023-03-23 16:31:11 +0000

    adding tech

--- pages/components/ExperienceCard.tsx
+++ pages/components/ExperienceCard.tsx
@@ -16,13 +16,31 @@ export default function ExperienceCard({}: Props) {
         alt="" 
       />
 
-      <div>
-        <h4>Junior Developer</h4>
-        <p>Cubicod</p>
-        <div>
-          {/* Tech used */}
-          {/* Tech used */}
-          {/* Tech used */}
+      <div className="px-0 md:px-10 ">
+        <h4 className="text-4xl font-light">Junior Developer</h4>
+        <p className="font-bold text-2xl mt-1">Cubicod</p>
+        <div className="flex space-x-2 my-2">
+           {/* Tech used */}
+          <img 
+            className="h-10 w-10 rounded-full"
+            src="https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5-thumbnail.png"
+            alt="" 
+          />
+          <img 
+            className="h-10 w-10 rounded-full"
+            src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png"
+            alt="" 
+          />
+          <img 
+            className="h-10 w-10 rounded-full"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png"
+            alt="" 
+          />
+          <img 
+            className="h-10 w-10 rounded-full"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Wordpress_logo_8.png"
+            alt="" 
+          />
         </div>
         <p>Started work...- Ended...</p>
         <ul className="list-disc space-y-4 ml-5 text-lg">
