27f47465390685b8fb29202ac2f63438f52e7a08
simoneya <yavar.simona@yahoo.com>
2023-03-24 14:03:30 +0000

    working on skill component

---
+++ pages/components/Skill.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+type Props = {
+    directionLeft?: boolean;
+}
+
+export default function Skill({ directionLeft }: Props) {
+  return (
+    <div className="group relative flex cursor-pointer">
+        <motion.img 
+          initial={{
+               x: directionLeft ? -200 : 200, 
+               opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0 }}
+        />
+    </div>
+  )
+}
