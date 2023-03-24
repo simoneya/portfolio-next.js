eef05e949868a053dad7316d01ff0d2ecc52cbe0
simoneya <yavar.simona@yahoo.com>
2023-03-24 13:04:11 +0000

    update

--- pages/components/Experience.tsx
+++ pages/components/Experience.tsx
@@ -10,7 +10,7 @@ export default function Experience({}: Props) {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
-        className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+        className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 pt-20 lg:pt-40 justify-evenly mx-auto items-center"
         >
         <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">
             Experience
