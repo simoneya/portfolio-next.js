3941a7a443c36e6318eba54fb81db779a1231303
simoneya <yavar.simona@yahoo.com>
2023-03-26 17:04:27 +0100

    working on line

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -5,7 +5,11 @@ type Props = {}
 export default function Projects({}: Props) {
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
-        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">Projects</h3>
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">
+            Projects
+        </h3>
+
+        <div className="w-full absolute top-[30%] bg-[#060047]/10 left-0 h-[500px] -skew-y-12" />
     </div>
   );
 }
