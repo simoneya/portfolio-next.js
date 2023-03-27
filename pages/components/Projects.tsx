6e81dbd2e6130fb2d179ed4de51485a62e27be40
simoneya <yavar.simona@yahoo.com>
2023-03-27 10:44:30 +0100

    adding project to projects section

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 type Props = {}
 
 export default function Projects({}: Props) {
+
+    const projects = [1, 2, 3, 4, 5];
   return (
     <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
         <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">
@@ -10,10 +12,18 @@ export default function Projects({}: Props) {
         </h3>
 
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-            {/* Project */}
-            {/* Project */}
-            {/* Project */}
-            {/* Project */}
+            {projects.map((project) => (
+                <div className="w-screen flex-shrink-0 snap-center">
+                    <img 
+                      src="https://whatsondisneyplus.com/wp-content/uploads/2019/11/disneyplusmobile_home_ea3b0a6a-e1573467317313.png" 
+                      alt=""
+                    />
+
+                    <div>
+                        <h4>Case study 1 of 2 Disney+ clone</h4>
+                    </div>
+                </div>
+            ))}
         </div>
 
         <div className="w-full absolute top-[30%] bg-[#060047]/10 left-0 h-[500px] -skew-y-12" />
