47e96d46a933ed352be4cd6cc2b0a4445e5c0da7
simoneya <yavar.simona@yahoo.com>
2023-03-27 11:07:37 +0100

    update

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -11,15 +11,15 @@ export default function Projects({}: Props) {
             Projects
         </h3>
 
-        <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 lg:top-14">
             {projects.map((project, i) => (
-                <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+                <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 lg:p-64 h-screen">
                     <img 
                       src="https://whatsondisneyplus.com/wp-content/uploads/2019/11/disneyplusmobile_home_ea3b0a6a-e1573467317313.png" 
-                      alt=""
+                      alt="project-image"
                     />
 
-                    <div>
+                    <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                         <h4 className="text-4xl font-semibold text-center">
                             <span className="underline decoration-[#5AD1AF]/50">
                                Case study {i+1} of {projects.length} :
