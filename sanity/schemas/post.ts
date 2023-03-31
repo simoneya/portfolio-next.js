3d3f1d61c0c396336f70e2fafe986993c40d1b90
simoneya <yavar.simona@yahoo.com>
2023-03-31 13:32:24 +0100

    update

--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -5,6 +5,36 @@ export default defineType({
   title: 'PageInfo',
   type: 'document',
   fields: [
-
+    {
+       name: "name",
+       title: "Name",
+       type: "string",
+    },
+    {
+       name: "role",
+       title: "Role",
+       type: "string",
+   },
+   {
+       name: "heroImage",
+       title: "Image",
+       type: "image",
+         options: {
+           hotspot: true,
+         },
+   },
+   {
+      name: "backgroundInformation",
+      title: "BackgroundInformation",
+      type: "string",
+   },
+   {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+        options: {
+            hotspot: true,
+        },
+   },
   ],
 })
