8f6d57207c9bade21ed08ec27995081acfe29666
simoneya <yavar.simona@yahoo.com>
2023-03-31 13:21:11 +0100

    installing sanity to the project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hkl4j6o3',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/post.ts
@@ -0,0 +1,10 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'PageInfo',
+  type: 'document',
+  fields: [
+
+  ],
+})
