9b990842d8e9c47cc01f311219a46a2e5407f483
simoneya <yavar.simona@yahoo.com>
2023-04-01 16:19:46 +0100

    filling database info

--- sanity/schemas/experience.ts
+++ sanity/schemas/experience.ts
@@ -1,40 +1,37 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'post',
-  title: 'PageInfo',
+  name: 'experience',
+  title: 'Experience',
   type: 'document',
   fields: [
     {
-       name: "name",
-       title: "Name",
+       name: "jobTitle",
+       title: "Jobtitle",
        type: "string",
     },
     {
-       name: "role",
-       title: "Role",
-       type: "string",
-   },
-   {
-       name: "heroImage",
-       title: "Image",
+       name: "companyImage",
+       title: "CompanyImage",
        type: "image",
          options: {
-           hotspot: true,
+            hotspot: true,
          },
    },
    {
-      name: "backgroundInformation",
-      title: "BackgroundInformation",
-      type: "string",
+       name: "company",
+       title: "Company",
+       type: "text",
    },
    {
-      name: "profilePic",
-      title: "ProfilePic",
-      type: "image",
-        options: {
-            hotspot: true,
-        },
+      name: "dateStarted",
+      title: "DateStarted",
+      type: "date",
+   },
+   {
+      name: "dateEnded",
+      title: "DateEnded",
+      type: "date",
    },
    {
       name: "phoneNumber",
@@ -42,9 +39,9 @@ export default defineType({
       type: "string",
    },
    {
-      name: "email",
-      title: "Email",
-      type: "string",
+      name: "isCurrentlyWorkingHere",
+      title: "IsCurrentlyWorkingHere",
+      type: "boolean",
    },
    {
       name: "address",
@@ -52,10 +49,16 @@ export default defineType({
       type: "string",
    },
    {
-      name: "socials",
-      title: "Socials",
+      name: "technologies",
+      title: "Technologies",
       type: "array",
-      of: [{ type: "reference", to: { type: "social" } }],
+      of: [{ type: "reference", to: { type: "skill" } }],
+   },
+   {
+    name: "points",
+    title: "Points",
+    type: "array",
+    of: [{ type: "string" }],
    },
   ],
 })
