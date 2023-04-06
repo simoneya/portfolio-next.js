02c6a703fb2b2857533f4202fd2c0750c5ec17ea
simoneya <yavar.simona@yahoo.com>
2023-04-06 17:45:00 +0100

    update

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -10,9 +10,12 @@ type Data = {
     socials: Social[]
 }
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
   ) {
+
+    const socials: Social[] = await sanityClient.fetch(query);
+    
     res.status(200).json({ name: 'John Doe' })
   }
