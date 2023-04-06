48d43be4272075ecf422a0dd245b24547b536e89
simoneya <yavar.simona@yahoo.com>
2023-04-06 17:53:22 +0100

    adding interfaces

--- typings.d.ts
+++ typings.d.ts
@@ -5,6 +5,14 @@ interface SanityBody {
     _updateAt: string;
 }
 
+interface Image {
+    _type: "image";
+    asset: {
+        _ref: string;
+        _type: "reference";
+    }
+}
+
 export interface Social extends SanityBody {
     _type: "social";
     title: string;
