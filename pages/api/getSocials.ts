582d78bd429f60686aee152899993efc1c9a9b8b
simoneya <yavar.simona@yahoo.com>
2023-04-06 17:41:17 +0100

    update

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,3 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
+
+const query = groq`
+    *[_type == "social"]
+`
+
+type Data = {
+    socials: Social[]
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+  ) {
+    res.status(200).json({ name: 'John Doe' })
+  }
