6301caaebf98d177b1ea85a45efbdf66c0a3dfbb
simoneya <yavar.simona@yahoo.com>
2023-04-07 17:44:15 +0100

    adding other interfaces

--- typings.d.ts
+++ typings.d.ts
@@ -10,7 +10,42 @@ interface Image {
     asset: {
         _ref: string;
         _type: "reference";
-    }
+    };
+}
+
+export interface PageInfo extends SanityBody {
+    _type: "pageInfo";
+    address: string;
+    backgroundInformation: string;
+    email: string;
+    role: string;
+    heroImage: Image;
+    name: string;
+    phoneNumber: string;
+    profilePic: Image;
+}
+
+export interface Technology extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Project extends SanityBody {
+    title: string;
+    _type: "project";
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];
 }
 
 export interface Social extends SanityBody {
