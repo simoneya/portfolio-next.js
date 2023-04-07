1c48b0ef972e2b84575fe8c14c2c13ac8722bc3e
simoneya <yavar.simona@yahoo.com>
2023-04-07 17:50:16 +0100

    import typings data to getSocials file

--- pages/api/getSocials.ts
+++ pages/api/getSocials.ts
@@ -1,14 +1,15 @@
+import { Social } from "@/typings";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
 
 const query = groq`
     *[_type == "social"]
-`
+`;
 
 type Data = {
-    socials: Social[]
-}
+    socials: Social[];
+};
 
 export default async function handler(
     req: NextApiRequest,
