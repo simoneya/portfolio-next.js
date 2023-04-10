1d805ef28f7691026d0e65a3400d0b4235a94cea
simoneya <yavar.simona@yahoo.com>
2023-04-10 14:12:57 +0100

    update

--- tan-alpaca/utils/fetchSkills.ts
+++ tan-alpaca/utils/fetchSkills.ts
@@ -1,9 +1,14 @@
 import { Skill } from "@/typings";
 import { RestoreIcon } from "@sanity/icons";
+import { ConfigResolutionError } from "sanity";
 
 export const fetchSkills = async() => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api.getSkills`);
 
     const data = await res.json();
     const skills: Skill[] = data.skills;
+
+    //console.log("fetching", skills);
+
+    return skills;
 }
