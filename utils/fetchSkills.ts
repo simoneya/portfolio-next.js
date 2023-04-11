acc6d23eb465351a1c8f1f0646da3871e2c3372f
simoneya <yavar.simona@yahoo.com>
2023-04-11 18:18:49 +0100

    update

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,8 +1,6 @@
 import { Skill } from "@/typings";
-import { RestoreIcon } from "@sanity/icons";
-import { ConfigResolutionError } from "sanity";
 
-export const fetchSkills = async() => {
+export const fetchSkills = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api.getSkills`);
 
     const data = await res.json();
@@ -11,4 +9,4 @@ export const fetchSkills = async() => {
     //console.log("fetching", skills);
 
     return skills;
-}
+};
