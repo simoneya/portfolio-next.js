4afccef6d34158ff3493801a9e23bba2f8adc32d
simoneya <yavar.simona@yahoo.com>
2023-04-13 11:46:12 +0100

    update

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,6 +1,8 @@
+//our front-end //
 import { Skill } from "@/typings";
 
 export const fetchSkills = async () => {
+    // making a call to back-end code line bellow -> //
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
 
     const data = await res.json();
