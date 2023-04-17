774beff9ac25ac8215e5c6d02dcb70bcc6140ed5
simoneya <yavar.simona@yahoo.com>
2023-04-17 10:24:28 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ const Home = ({pageInfo, skills, projects, socials, experiences}: Props) => {
       </section>
 
       <section id="projects" className="snap-start">
-        <Projects />
+        <Projects projects={projects}/>
       </section>
 
       <section id="contact" className="snap-start">
