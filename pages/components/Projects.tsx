19ff6ee4540762cf083a373fb4dba13f4c45ba7b
simoneya <yavar.simona@yahoo.com>
2023-04-18 10:06:32 +0100

    update

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -40,7 +40,7 @@ export default function Projects({ projects }: Props) {
                         </h4>
 
                         {project?.technologies.map(technology => (
-                            <img key={technology._id} src="" alt="" />
+                            <img key={technology._id} src={urlFor(technology.image).url()} alt="" />
                         ))}
 
                         <p className="text-md text-center md:text-left lg:text-md">
