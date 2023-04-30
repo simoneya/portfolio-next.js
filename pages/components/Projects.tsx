a54de8a08a1b6ca34105a455daabed5f5976961f
simoneya <yavar.simona@yahoo.com>
2023-04-30 14:04:37 +0100

    update

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -21,7 +21,7 @@ export default function Projects({ projects }: Props) {
 
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 lg:top-14 scrollbar-thin scrollbar-thumb-stone-800 scrollbar-track-amber-400/20">
             {projects?.map((project, i) => (
-                <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 lg:p-64 h-screen">
+                <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 lg:p-150 h-screen">
                     <motion.img 
                       initial={{ y: -300, opacity: 0, }}
                       transition={{ duration: 1.2 }}
