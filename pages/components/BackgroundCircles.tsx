926719a65c165b1c87192bcbc48f488ac115cadb
simoneya <yavar.simona@yahoo.com>
2023-05-07 09:53:39 +0100

    update

--- pages/components/BackgroundCircles.tsx
+++ pages/components/BackgroundCircles.tsx
@@ -19,10 +19,10 @@ export default function BackgroundCircles({}: Props) {
       }}
         className="relative flex justify-center items-center"> 
         <div className="absolute border border-[#FF6000] rounded-full h-[200px] w-[200px] mt-52 animate-ping"/>
-        <div className="rounded-full border border-[#D8B9C3] h-[300px] w-[300px] absolute mt-52"/>
-        <div className="rounded-full border border-[#D8B9C3] h-[500px] w-[500px] absolute mt-52"/>
+        <div className="rounded-full border border-[#6D5D6E] h-[300px] w-[300px] absolute mt-52"/>
+        <div className="rounded-full border border-[#6D5D6E] h-[500px] w-[500px] absolute mt-52"/>
         <div className="rounded-full border border-[#454545] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse"/>
-        <div className="rounded-full border border-[#D8B9C3] h-[800px] w-[800px] absolute mt-52"/>
+        <div className="rounded-full border border-[#6D5D6E] h-[800px] w-[800px] absolute mt-52"/>
     </motion.div>
   );
 }
