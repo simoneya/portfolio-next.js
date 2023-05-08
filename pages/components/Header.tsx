661d927fc673b1431967d2df29127daf4969852a
simoneya <yavar.simona@yahoo.com>
2023-05-08 13:26:02 +0100

    update

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header({ socials }: Props) {
               <SocialIcon 
               key={social._id}
               url={social.url} 
-              fgColor="#413F42"  
+              fgColor="#7F8487"  
               bgColor="transparent" 
              />
             ))}
@@ -57,11 +57,11 @@ export default function Header({ socials }: Props) {
             <SocialIcon
              className="cursor-pointer"
              network="email"
-             fgColor="#413F42"  
+             fgColor="#7F8487"  
              bgColor="transparent"
             />
 
-            <p className="uppercase hidden md:inline-flex text-sm text-[#413F42]">Get In Touch</p>
+            <p className="uppercase hidden md:inline-flex text-sm text-[#7F8487]">Get In Touch</p>
         </motion.div>
       </Link>
 
