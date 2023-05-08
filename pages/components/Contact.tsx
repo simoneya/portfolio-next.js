49f62cf4825631aa80ccd777ecfef09a6f20e99f
simoneya <yavar.simona@yahoo.com>
2023-05-08 13:29:43 +0100

    update

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -30,22 +30,22 @@ export default function Contact({}: Props) {
          <div className="flex flex-col space-y-10">
              <h4 className="text-4xl font-semibold text-center">
                  I have got what you need.{" "}
-                 <span className="decoration-[#F55050]/50 underline">Lets Talk.</span>
+                 <span className="decoration-[#FF6000]/50 underline">Lets Talk.</span>
              </h4>
 
              <div className="space-y-10">
                  <div className="flex items-center space-x-5 justify-center">
-                     <PhoneIcon className="text-[#F55050] h-7b w-7 animate-pulse"/>
+                     <PhoneIcon className="text-[#FF6000] h-7b w-7 animate-pulse"/>
                      <p className="text-2xl">07542 383394</p>
                  </div>
 
                  <div className="flex items-center space-x-5 justify-center">
-                     <EnvelopeIcon className="text-[#F55050] h-7b w-7 animate-pulse"/>
+                     <EnvelopeIcon className="text-[#FF6000] h-7b w-7 animate-pulse"/>
                      <p className="text-2xl">yavar.simona@yahoo.com</p>
                  </div>
 
                  <div className="flex items-center space-x-5 justify-center">
-                     <MapPinIcon className="text-[#F55050] h-7b w-7 animate-pulse"/>
+                     <MapPinIcon className="text-[#FF6000] h-7b w-7 animate-pulse"/>
                      <p className="text-2xl">Liverpool, UK</p>
                  </div>
              </div>
