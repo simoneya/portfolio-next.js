4ae2fbeb29c004fd9809066500a421e1094133b4
simoneya <yavar.simona@yahoo.com>
2023-05-10 12:55:47 +0100

    update

--- pages/components/Craft.tsx
+++ pages/components/Craft.tsx
@@ -19,7 +19,7 @@ export default function Craft({ skill, directionLeft }: Props) {
           transition={{ duration: 1 }}
           whileInView={{ opacity: 1, x: 0 }}
           src={urlFor(skill?.image).url()}
-          className="rounded-full border border-[#2B2B28] object-cover h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+          className="rounded-full border border-[#9BA4B5] object-cover h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 filter group-hover:grayscale transition duration-300 ease-in-out"
         />
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
