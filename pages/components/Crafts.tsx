738d386df3765fdc62205722d5434441e06c3e92
simoneya <yavar.simona@yahoo.com>
2023-05-10 12:56:30 +0100

    update

--- pages/components/Crafts.tsx
+++ pages/components/Crafts.tsx
@@ -15,11 +15,11 @@ export default function Crafts({ skills }: Props) {
         transition={{ duration: 1.5 }}
         className="flex relative flex-col text-center pt-20 md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
         
-        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#2B2B28] text-2xl">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-[#9BA4B5] text-2xl">
             Skills
         </h3>
 
-        <h3 className=" absolute top-36 uppercase tracking-[3px] text-[#2B2B28] text-sm">
+        <h3 className=" absolute top-36 uppercase tracking-[3px] text-[#9BA4B5] text-sm">
             Hover over a skill for current proficiency
         </h3>
 
